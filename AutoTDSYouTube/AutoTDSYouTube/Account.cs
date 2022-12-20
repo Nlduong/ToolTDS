938cd54ef9ea99be4cfe5c508acbecc48022c70f
Duong Nguyen Linh (SM) <duonnguyen@deloitte.ca>
2022-12-20 11:19:37 +0700

    add code

---
+++ AutoTDSYouTube/AutoTDSYouTube/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoTTCYouTube
+{
+    internal class Account
+    {
+        public string idgg { get; set; }
+        public string passgg { get; set; }
+        public string idtds { get; set; }
+        public string passtds { get; set; }
+    }
+}
